87101c41119005382d3d12bbf4be9a1dde2ecc73
marc <marc@kprod.net>
2018-03-01 16:04:15 +0100

    basic event listing

--- src/main/java/net/kprod/aalarmui/controller/ControllerEvent.java
+++ src/main/java/net/kprod/aalarmui/controller/ControllerEvent.java
@@ -1,5 +1,6 @@
 package net.kprod.aalarmui.controller;
 
+import net.kprod.aalarmui.bean.Event;
 import net.kprod.aalarmui.service.ServiceEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by kemkem on 11/11/17.
@@ -32,5 +35,10 @@ public class ControllerEvent {
     @RequestMapping("/sensor")
     public void eventSensor(@RequestParam String sensor, @RequestParam String event) {
         serviceEvent.recordEventSensor(sensor, event);
+    }
+
+    @RequestMapping("/list")
+    public List<Event> list() {
+        return serviceEvent.listEventAll();
     }
 }
